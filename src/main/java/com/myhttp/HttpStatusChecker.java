5e1fa0bd1e4f619c989f6aa6b0453452a2ca3249
korvalanchik <korvalan@yahoo.com>
2024-01-11 16:16:06 +0200

    Version 1.0.0

---
+++ src/main/java/com/myhttp/HttpStatusChecker.java
@@ -0,0 +1,30 @@
+package com.myhttp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpStatusChecker {
+    public static String connectionTo(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        int responseCode = connection.getResponseCode();
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+        } else {
+            System.out.println("GET posts request not worked");
+        }
+        return String.valueOf(response);
+    }
+}
